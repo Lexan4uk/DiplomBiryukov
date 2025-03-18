9eee4d9f5babcde7b246a36390e2be07f37659e7
Alexey Bir <alkatras46rus@gmail.com>
2025-03-18 13:11:52 +0300

    comm

--- src/api/simpleGet.js
+++ src/api/simpleGet.js
@@ -1,25 +1,22 @@
-import { api } from "@api/api";
+import { api } from '@api/api'
 
 export const apiTags = {
-    getAddresses: "addresses/getAddresses",
-    getBoquetCompleted: "boquets/getBoquetCompleted",
-    getBoquetConstructor: "boquets/getBoquetConstructor",
-    getBoquetByLink: (link) => `boquets/getBoquetByLink/${link}`,
-    getReviews: "reviews/getReviews",
-    getOrdersDone: (name) => `orders/getOrdersDone/${name}`,
-    getOrders: "orders/getOrders",
-    getReviewsApproved: "reviews/getReviewsApproved",
-};
+	getAddresses: 'addresses/getAddresses',
+	getBoquetCompleted: 'boquets/getBoquetCompleted',
+	getBoquetByLink: link => `boquets/getBoquetByLink/${link}`,
+	getReviews: 'reviews/getReviews',
+	getOrdersDone: name => `orders/getOrdersDone/${name}`,
+	getOrders: 'orders/getOrders',
+	getReviewsApproved: 'reviews/getReviewsApproved',
+}
 
 export async function simpleGet(params) {
-    if (params) {
-        const response = await api.get(params);
-        return response.data;
-    }
-    else {
-        console.log("Неверная форма запроса")
-        return null
-    }
-
+	if (params) {
+		const response = await api.get(params)
+		return response.data
+	} else {
+		console.log('Неверная форма запроса')
+		return null
+	}
 }
 export default simpleGet
--- src/components/popups/ProfileMenuPopover.jsx
+++ src/components/popups/ProfileMenuPopover.jsx
@@ -1,43 +1,48 @@
-import '@styles/popups/PofileMenuPopover.scss';
-import { PopoverPanel } from '@headlessui/react'
-import { AnimatePresence, motion } from 'framer-motion'
-import getSvg from '@images/svg'
 import ProfileName from '@components/elements/profile_menu_elements/ProfileName'
-import ProfilePhone from '@components/elements/profile_menu_elements/ProfilePhone'
 import ProfileNewPassword from '@components/elements/profile_menu_elements/ProfileNewPassword'
+import ProfilePhone from '@components/elements/profile_menu_elements/ProfilePhone'
+import { PopoverPanel } from '@headlessui/react'
+import getSvg from '@images/svg'
+import '@styles/popups/PofileMenuPopover.scss'
+import { AnimatePresence, motion } from 'framer-motion'
 
-import useAuth from '@scripts/custom_hooks/useAuth';
+import useAuth from '@scripts/custom_hooks/useAuth'
 
 function ProfileMenuPopover() {
-  const {
-    pen,
-    cross
-  } = getSvg()
-  const {
-    accData,
-    initUser } = useAuth();
-  const exitAcc = () => {
-    localStorage.removeItem('token');
-    initUser()
-  }
+	const { pen, cross } = getSvg()
+	const { accData, initUser } = useAuth()
+	const exitAcc = () => {
+		localStorage.removeItem('token')
+		initUser()
+	}
 
-  return (
-    <AnimatePresence>
-      <PopoverPanel
-        as={motion.div}
-        initial={{ opacity: 0, scale: 0.95 }}
-        animate={{ opacity: 1, scale: 1 }}
-        exit={{ opacity: 0, scale: 0.95 }} anchor="bottom end" className="profile-menu-popover profile-menu-popover_props">
-        <div className="profile-menu-popover__holder f-column gap-16">
-          <h2 className="profile-menu-popover__title text-m">Данные Вашего аккаунта<br /> будут использоваться для оформления заказа</h2>
-          <ProfileName name={accData?.name} />
-          <ProfilePhone phone={accData?.phoneNumber} />
-          <ProfileNewPassword />
-          <button className='profile-menu-popover__exit-btn simple-button text-red' onClick={exitAcc}>Выйти из профиля</button>
-        </div>
-
-      </PopoverPanel>
-    </AnimatePresence>
-  )
+	return (
+		<AnimatePresence>
+			<PopoverPanel
+				as={motion.div}
+				initial={{ opacity: 0, scale: 0.95 }}
+				animate={{ opacity: 1, scale: 1 }}
+				exit={{ opacity: 0, scale: 0.95 }}
+				anchor='bottom end'
+				className='profile-menu-popover profile-menu-popover_props'
+			>
+				<div className='profile-menu-popover__holder f-column gap-16'>
+					<h2 className='profile-menu-popover__title text-m'>
+						Данные Вашего аккаунта
+						<br /> будут использоваться для оформления заказа
+					</h2>
+					<ProfileName name={accData?.name} />
+					<ProfilePhone phone={accData?.phoneNumber} />
+					<ProfileNewPassword />
+					<button
+						className='profile-menu-popover__exit-btn simple-button text-red'
+						onClick={exitAcc}
+					>
+						Выйти
+					</button>
+				</div>
+			</PopoverPanel>
+		</AnimatePresence>
+	)
 }
 export default ProfileMenuPopover
